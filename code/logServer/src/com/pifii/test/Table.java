c3f9cd8e1a3670ba1800fe11b07f6cbd42a35def
Administrator <1152695512@qq.com>
2015-07-07 18:33:30 +0800

    ss

--- code/logServer/src/com/pifii/test/Table.java
+++ code/logServer/src/com/pifii/test/Table.java
@@ -21,14 +21,14 @@ public class Table extends TimerTask {
 			log.debug("开始分表+++++++++++++++++++++++++++");
 			CutTableJdbc.reamTableName(true, ResourcesUtil.getVbyKey("tablename"), data);
 			log.debug("结束分表+++++++++++++++++++++++++++");
-			if(CutTableJdbc.isdureHere(ResourcesUtil.getVbyKey("tablename")+data,DateUtil.getTabDay())){
+			if(CutTableJdbc.isdureHere(ResourcesUtil.getVbyKey("tablename")+data,DateUtil.getTabDay())){/*
 				log.debug("表中有其他日期的值+++++++++++++++++++++++++++"+DateUtil.getTabYesterDay());
 				List<String> lst=CutTableJdbc.getReplace(ResourcesUtil.getVbyKey("tablename")+data,DateUtil.getTabDay());
 				for (String string : lst) {
 					System.out.println(ResourcesUtil.getVbyKey("tablename")+data+":中存在的其他日期："+string);
 					log.error(ResourcesUtil.getVbyKey("tablename")+data+":中存在的其他日期："+string);
-					/*CutTableJdbc.insertTab(ResourcesUtil.getVbyKey("tablename")+DateUtil.getTabCurrentDay(string),ResourcesUtil.getVbyKey("tablename")+data,string);
-					CutTableJdbc.deleteDureTab(ResourcesUtil.getVbyKey("tablename")+data,string);*/
+					CutTableJdbc.insertTab(ResourcesUtil.getVbyKey("tablename")+DateUtil.getTabCurrentDay(string),ResourcesUtil.getVbyKey("tablename")+data,string);
+					CutTableJdbc.deleteDureTab(ResourcesUtil.getVbyKey("tablename")+data,string);
 					String tabData=DateUtil.getTabDay();
 					if(!tabData.equals(string)){
 						String val=string.replace("-", "_");
@@ -44,17 +44,17 @@ public class Table extends TimerTask {
 						log.debug("已经删除了表中的其他日期+++++++++++++++++++++++++++");
 					}
 				}
-			}	
+			*/}	
 		}
 		
-		if(CutTableJdbc.isdureHere(ResourcesUtil.getVbyKey("tablename")+data,DateUtil.getTabDay())){
+/*		if(CutTableJdbc.isdureHere(ResourcesUtil.getVbyKey("tablename")+data,DateUtil.getTabDay())){
 			log.debug("表中有其他日期的值+++++++++++++++++++++++++++"+DateUtil.getTabYesterDay());
 			List<String> lst=CutTableJdbc.getReplace(ResourcesUtil.getVbyKey("tablename")+data,DateUtil.getTabDay());
 			for (String string : lst) {
 				System.out.println(ResourcesUtil.getVbyKey("tablename")+data+":中存在的其他日期："+string);
 				log.error(ResourcesUtil.getVbyKey("tablename")+data+":中存在的其他日期："+string);
-				/*CutTableJdbc.insertTab(ResourcesUtil.getVbyKey("tablename")+DateUtil.getTabCurrentDay(string),ResourcesUtil.getVbyKey("tablename")+data,string);
-				CutTableJdbc.deleteDureTab(ResourcesUtil.getVbyKey("tablename")+data,string);*/
+				CutTableJdbc.insertTab(ResourcesUtil.getVbyKey("tablename")+DateUtil.getTabCurrentDay(string),ResourcesUtil.getVbyKey("tablename")+data,string);
+				CutTableJdbc.deleteDureTab(ResourcesUtil.getVbyKey("tablename")+data,string);
 				String tabData=DateUtil.getTabDay();
 				if(!tabData.equals(string)){
 					String val=string.replace("-", "_");
@@ -71,7 +71,7 @@ public class Table extends TimerTask {
 				}
 			}
 		}
-		}
+*/		}
 	}
 
 			
